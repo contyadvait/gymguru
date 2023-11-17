442116c29a16b75f3b31ac8b349925538f0caa1c
contyadvait <advaitthepro@outlook.com>
2023-11-17 11:02:14 +0800

    Initial Commit

---
+++ gymguru/gymguruApp.swift
@@ -0,0 +1,32 @@
+//
+//  gymguruApp.swift
+//  gymguru
+//
+//  Created by Milind Contractor on 17/11/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct gymguruApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
