8e5872bf4927585b4c0ea5c4ff51d6ad577b3124
contyadvait <advaitthepro@outlook.com>
2023-11-18 09:25:31 +0800

    Added first 2 pages

---
+++ gymguru/Setup Views/Pages.swift
@@ -0,0 +1,18 @@
+//
+//  Pages.swift
+//  gymguru
+//
+//  Created by Milind Contractor on 17/11/23.
+//
+
+import SwiftUI
+
+struct Pages: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Pages()
+}
