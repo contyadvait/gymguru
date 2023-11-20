ffff9e476eb750d1193ee6d5c84fe11cdee3b91a
contyadvait <advaitthepro@outlook.com>
2023-11-20 09:35:01 +0800

    Added Tabs

---
+++ gymguru/TabItems/HomeView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+#Preview {
+    HomeView()
+}
