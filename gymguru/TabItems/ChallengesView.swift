5f4bb8fb34ea5a59a39a61256792b43d84faab1c
contyadvait <advaitthepro@outlook.com>
2023-11-22 15:31:51 +0800

    Added SDWebImage support

---
+++ gymguru/TabItems/ChallengesView.swift
@@ -0,0 +1,18 @@
+//
+//  ChallengesView.swift
+//  gymguru
+//
+//  Created by Milind Contractor on 22/11/23.
+//
+
+import SwiftUI
+
+struct ChallengesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChallengesView()
+}
