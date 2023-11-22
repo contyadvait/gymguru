b366f758c59b06a3d20591a6023a6ff98b2093c4
contyadvait <advaitthepro@outlook.com>
2023-11-22 14:12:10 +0800

    Quick fix with names OOPS 🤭

---
+++ gymguru/TabItems/BadgesView.swift
@@ -0,0 +1,18 @@
+//
+//  BadgesView.swift
+//  gymguru
+//
+//  Created by Milind Contractor on 22/11/23.
+//
+
+import SwiftUI
+
+struct BadgesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgesView()
+}
