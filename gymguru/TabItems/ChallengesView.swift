40da58a30b97f94a6fc8f0d49f7e434405221e34
brianj <brian_joseph@student.edu.sg>
2023-11-27 10:16:33 +0800

    Added location list :D

--- gymguru/TabItems/ChallengesView.swift
+++ gymguru/TabItems/ChallengesView.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct ChallengesView: View {
-    @State var challengesAvailable = [ChallengeData(challengeName: "Christmas Calorie Loss", challengeDescription: "Lose some calories to consume extra for christmas!", challengeItems: [ExerciseItem(workoutItem: .burpee, workoutTrackType: .counter, amount: 10)]),
-                                      ChallengeData(challengeName: "New Year Goals Meet", challengeDescription: "Meet your exercise-related goals just in time for the New Year!", challengeItems: [ExerciseItem(workoutItem: .burpee, workoutTrackType: .counter, amount: 10)])]
+    @State var challengesAvailable = [ChallengeData(challengeName: "Christmas Calorie Loss", challengeDescription: "Lose some calories to consume extra for christmas!", challengeItems: [ExerciseItem(workoutItem: .burpee, workoutTrackType: .counter, amount: 10)], badges: []),
+                                      ChallengeData(challengeName: "New Year Goals Meet", challengeDescription: "Meet your exercise-related goals just in time for the New Year!", challengeItems: [ExerciseItem(workoutItem: .burpee, workoutTrackType: .counter, amount: 10)], badges: <#[Badge]#>)],
     @Environment(\.colorScheme) var colorScheme
     @Binding var userData: UserInfo
     @State var customChallenge = false
