984b2648ec82a829eb7682811d2427d3e672e794
brianj <brian_joseph@student.edu.sg>
2023-11-30 18:55:34 +0800

    Added all the badges needed

--- gymguru/DailyChallenges.swift
+++ gymguru/DailyChallenges.swift
@@ -15,21 +15,21 @@ class ChallengeManager {
     func reRoll(userData: UserInfo, challengeStreak: Int) -> ChallengeData {
         if userData.preferredWorkouts == [] {
             if challengeStreak == 7 {
-                return ChallengeData(challengeName: "The Daily Challenge", challengeDescription: "Refreshes every day", challengeItems: [ExerciseItem(workoutItem: Exercise.random(), amount: Float(Int.random(in: 5...10)))], badges: [Badge(badge: "Week Streak Maintainer", sfIcon: "7.circle", obtainingExercise: .cycling, amountOfObtainingExercise: 10, obtained: false)])
+                return ChallengeData(challengeName: "Today's Challenge", challengeDescription: "Refreshes every day", challengeItems: [ExerciseItem(workoutItem: Exercise.random(), amount: Float(Int.random(in: 5...10)))], badges: [Badge(badge: "Week Streak Maintainer", sfIcon: "7.circle", obtainingExercise: .cycling, amountOfObtainingExercise: 10, obtained: false)])
                 
             } else if challengeStreak == 30 {
-                return ChallengeData(challengeName: "The Daily Challenge", challengeDescription: "Refreshes every day", challengeItems: [ExerciseItem(workoutItem: Exercise.random(), amount: Float(Int.random(in: 5...10)))], badges: [Badge(badge: "Month Streak Maintainer", sfIcon: "30.circle", obtainingExercise: .cycling, amountOfObtainingExercise: 10, obtained: false)])
+                return ChallengeData(challengeName: "Today's Challenge", challengeDescription: "Refreshes every day", challengeItems: [ExerciseItem(workoutItem: Exercise.random(), amount: Float(Int.random(in: 5...10)))], badges: [Badge(badge: "Month Streak Maintainer", sfIcon: "30.circle", obtainingExercise: .cycling, amountOfObtainingExercise: 10, obtained: false)])
             } else {
-                return ChallengeData(challengeName: "The Daily Challenge", challengeDescription: "Refreshes every day", challengeItems: [ExerciseItem(workoutItem: Exercise.random(), amount: Float(Int.random(in: 5...10)))], badges: [])
+                return ChallengeData(challengeName: "Today's Challenge", challengeDescription: "Refreshes every day", challengeItems: [ExerciseItem(workoutItem: Exercise.random(), amount: Float(Int.random(in: 5...10)))], badges: [])
             }
         } else {
             if challengeStreak == 7 {
-                return ChallengeData(challengeName: "The Daily Challenge", challengeDescription: "Refreshes every day", challengeItems: [ExerciseItem(workoutItem: userData.preferredWorkouts.randomElement() ?? Exercise.random(), amount: Float(Int.random(in: 5...10)))], badges: [Badge(badge: "Week Streak Maintainer", sfIcon: "7.circle", obtainingExercise: .cycling, amountOfObtainingExercise: 10, obtained: false)])
+                return ChallengeData(challengeName: "Today's Challenge", challengeDescription: "Refreshes every day", challengeItems: [ExerciseItem(workoutItem: userData.preferredWorkouts.randomElement() ?? Exercise.random(), amount: Float(Int.random(in: 5...10)))], badges: [Badge(badge: "Week Streak Maintainer", sfIcon: "7.circle", obtainingExercise: .cycling, amountOfObtainingExercise: 10, obtained: false)])
                 
             } else if challengeStreak == 30 {
-                return ChallengeData(challengeName: "The Daily Challenge", challengeDescription: "Refreshes every day", challengeItems: [ExerciseItem(workoutItem: userData.preferredWorkouts.randomElement() ?? Exercise.random(), amount: Float(Int.random(in: 5...10)))], badges: [Badge(badge: "Month Streak Maintainer", sfIcon: "30.circle", obtainingExercise: .cycling, amountOfObtainingExercise: 10, obtained: false)])
+                return ChallengeData(challengeName: "Today's Challenge", challengeDescription: "Refreshes every day", challengeItems: [ExerciseItem(workoutItem: userData.preferredWorkouts.randomElement() ?? Exercise.random(), amount: Float(Int.random(in: 5...10)))], badges: [Badge(badge: "Month Streak Maintainer", sfIcon: "30.circle", obtainingExercise: .cycling, amountOfObtainingExercise: 10, obtained: false)])
             } else {
-                return ChallengeData(challengeName: "The Daily Challenge", challengeDescription: "Refreshes every day", challengeItems: [ExerciseItem(workoutItem: userData.preferredWorkouts.randomElement() ?? Exercise.random(), amount: Float(Int.random(in: 5...10)))], badges: [])
+                return ChallengeData(challengeName: "Today's Challenge", challengeDescription: "Refreshes every day", challengeItems: [ExerciseItem(workoutItem: userData.preferredWorkouts.randomElement() ?? Exercise.random(), amount: Float(Int.random(in: 5...10)))], badges: [])
             }
         }
     }
